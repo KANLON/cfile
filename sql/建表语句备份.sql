62ba87b33010cb463c13868c3ad982ed369f4f16
kanlon <Canlong2015@126.com>
2022-10-06 23:51:42 +0800

    refactor: 完善初始化的sql

--- sql/建表语句备份.sql
+++ sql/建表语句备份.sql
@@ -1,13 +1,13 @@
 -- 创建收集文件数据库
-CREATE DATABASE `cfile` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE  /*!32312 IF NOT EXISTS*/ `cfile` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 USE cfile;
 
-DROP TABLE teacher ;
+DROP TABLE IF EXISTS teacher ;
 
 -- 创建教师用户表
 DROP TABLE IF EXISTS teacher;
-CREATE TABLE teacher(
+CREATE TABLE IF NOT EXISTS teacher(
   uid INT(11) PRIMARY KEY AUTO_INCREMENT COMMENT '主键自增id',  
   username VARCHAR(20) NOT NULL COMMENT '用户名',
   PASSWORD CHAR(32) NOT NULL COMMENT '密码',
@@ -21,7 +21,7 @@ CREATE TABLE teacher(
 ) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='教师用户表';
 
 -- 插入一条默认用户数据
-INSERT INTO teacher(username,PASSWORD,salt,nickname,email,ctime) VALUES('admin','ea48576f30be1669971699c09ad05c94','123456','默认用户','s19961234@126.com','2018-11-28 23:14:00')
+INSERT INTO teacher(username,PASSWORD,salt,nickname,email,ctime) VALUES('admin','ea48576f30be1669971699c09ad05c94','123456','默认用户','s19961234@126.com','2018-11-28 23:14:00');
 -- 截断表
 TRUNCATE TABLE teacher;
 -- 查询表
@@ -30,7 +30,7 @@ SELECT * FROM teacher;
 USE cfile;
 -- 创建任务表
 DROP TABLE IF EXISTS `task`;
-CREATE TABLE task(
+CREATE TABLE IF NOT EXISTS  task(
   tid INT(11) PRIMARY KEY AUTO_INCREMENT COMMENT '主键自增id', 
   uid INT(11) NOT NULL COMMENT '对应的用户id', 
   task_name VARCHAR(30) NOT NULL COMMENT '任务名',
@@ -40,7 +40,7 @@ CREATE TABLE task(
   submiting_num INT(11) DEFAULT '0' NOT NULL COMMENT '已经提交的人数',
   remark VARCHAR(500) DEFAULT '' NOT NULL COMMENT '备注',
   authentication TINYINT(1) DEFAULT '0' NOT NULL COMMENT '是否经过验证',
-  submiting_list VARCHAR(100000) COMMENT '提交了名单',
+  submiting_list text COMMENT '提交了名单',
   -- 由于不能同时创建两个default timestamp默认值所以将创建时间的默认值修改为'0000-00-00 00:00:00'
   ctime TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
   mtime TIMESTAMP NOT NULL DEFAULT NOW() COMMENT '修改时间',
@@ -57,7 +57,7 @@ SELECT * FROM task;
 
 SELECT * FROM teacher;
 
-SELECT COUNT(*) num  FROM task WHERE uid=1 AND task_name='测试任务'
+SELECT COUNT(*) num  FROM task WHERE uid=1 AND task_name='测试任务';
 
 -- 更新提交的人数
 UPDATE task SET  submiting_num=0 WHERE tid=1;
